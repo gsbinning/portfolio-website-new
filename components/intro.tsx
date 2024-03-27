a4438fd17b41ac02506aeaeb063ea2deb0ae0cd0
gsbinning <gugandeep.s@hotmail.com>
2024-03-27 16:04:43 +0000

    got rid of C and added new photo

--- components/intro.tsx
+++ components/intro.tsx
@@ -34,7 +34,7 @@ export default function Intro() {
             }}
           >
             <img
-            src="https://media.licdn.com/dms/image/D4E03AQFUsghre4QfiQ/profile-displayphoto-shrink_800_800/0/1661686521725?e=1706140800&v=beta&t=qFSx1Uk-96jXwg4wDgcgTUICU9R3p6o3Vjc47Ks86NQ"
+            src="https://media.licdn.com/dms/image/D4E03AQE7weg36oR5aA/profile-displayphoto-shrink_800_800/0/1710252044023?e=1717027200&v=beta&t=5cHIMmnkHphOBtrZ3BM7G5ffVBLT3yxqnGsLSEPzXTE"
             alt="Gugandeep portrait"
               width="192"
               height="192"
@@ -92,15 +92,7 @@ export default function Intro() {
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
 
-        <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Frontend_CV.pdf"
-          download
-        >
-          Download CV{" "}
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </a>
-
+     
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://www.linkedin.com/in/gugandeepbinning/"
